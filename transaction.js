c24f1969215bbaed7cd79b3d4f32cd0bcba75ea8
Marina Ivanova <ubcg62bs@Marinas-MBP.home>
2022-10-24 14:36:00 +0100

    Date and debit transactions

--- transaction.js
+++ transaction.js
@@ -1,6 +1,12 @@
 class Transaction {
-  constructor(value) {
+  constructor(value, date) {
     this.value = value
+    this.date = date
+    this.debit = false
+  }
+
+  isDebit() {
+    this.value = this.value * (-1)
   }
 }
 
--- transaction.test.js
+++ transaction.test.js
@@ -1,8 +1,15 @@
 const Transaction = require('./transaction')
 
 describe('Transaction', () => {
-  it('saves a transaction and can return it', () => {
-    const transaction1 = new Transaction(300);
+  it('saves a transaction and can return its value and date', () => {
+    const transaction1 = new Transaction(300, '24/10/2022');
     expect(transaction1.value).toEqual(300)
+    expect(transaction1.date).toEqual('24/10/2022')
+  })
+
+  it('can be set as a debit transaction', () => {
+    const transaction1 = new Transaction(300, '24/10/2022');
+    transaction1.isDebit()
+    expect(transaction1.value).toEqual(-300)
   })
 })
